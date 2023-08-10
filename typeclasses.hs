f924cc987054376109fb89e16f33ad5ba3a4a71b
Michael Arntzenius <daekharel@gmail.com>
2023-08-10 18:55:48 +0100

    more work on typeclasses.hs

--- typeclasses.hs
+++ typeclasses.hs
@@ -1,5 +1,8 @@
 module Typeclasses where
 
+import Prelude hiding (head, tail)
+import Control.Applicative
+
 data Eventual a = Now a | Later (Eventual a) deriving (Show, Functor)
 
 class Later a where
@@ -21,36 +24,47 @@ eventual f (Later x) = later $ eventual f x
 -- fromEventual :: Later a => Eventual a -> a
 -- fromEventual = eventual id
 
-class Merge2 a where merge2 :: a -> a -> a
-class (Later a, Merge2 a) => Ocpo a where
-  merge  :: [a] -> a
-  merge = foldr merge2 never
+class (Later a, Semigroup a) => Ocpo a where
+  merge :: [a] -> a
+  merge [] = never
+  merge xs = foldr1 (<>) xs
 
-instance (Merge2 a, Merge2 b) => Merge2 (a, b) where merge2 (a,x) (b,y) = (merge2 a b, merge2 x y)
-instance (Ocpo a, Ocpo b) => Ocpo (a, b) where merge xys = (merge (map fst xys), merge (map snd xys))
+instance (Ocpo a, Ocpo b) => Ocpo (a, b) where
+  merge xys = (merge (map fst xys), merge (map snd xys))
 
-instance (Merge2 a, Merge2 b, Later a, Later b) => Merge2 (Eventual (Either a b)) where
-  merge2 (Later x) (Later y) = Later $ merge2 x y
-  merge2 x@Later{} y@Now{} = merge2 y x -- always put Now first
-  merge2 (Now (Left x)) y = Now $ Left $ merge2 x $ eventual getLeft y
+instance (Semigroup a, Semigroup b, Later a, Later b) => Semigroup (Eventual (Either a b)) where
+  Later x <> Later y = Later $ x <> y
+  x@Later{} <> y@Now{} = (<>) y x -- always put Now first
+  Now (Left x) <> y = Now $ Left $ (<>) x $ eventual getLeft y
     where getLeft (Left y) = y
           getLeft Right{} = error "Left/Right conflict"
-  merge2 (Now (Right x)) y = Now $ Right $ merge2 x $ eventual getRight y
+  Now (Right x) <> y = Now $ Right $ (<>) x $ eventual getRight y
     where getRight (Right y) = y
           getRight Left{} = error "Right/Left conflict"
-
 instance (Ocpo a, Ocpo b) => Ocpo (Eventual (Either a b)) where
 
 -- monotonically increasing infinite streams
--- NB. only a Functor for _monotone_ functions.
-data Stream a = Cons a (Stream a) deriving (Show, Functor)
+-- NB. only Functor/Applicative for _monotone_ functions.
+data Stream a = (:.) a (Stream a) deriving (Show, Functor)
 instance Applicative Stream where
-  pure x = xs where xs = Cons x xs
-  -- NB. assumes the stream of functions is monotonically increasing *and* that they are
-  -- all monotone!
-  Cons f fs <*> Cons x xs = Cons (f x) (fs <*> xs)
+  pure x = xs where xs = x :. xs
+  liftA2 f (x :. xs) (y :. ys) = f x y :. liftA2 f xs ys -- f must be monotone
+
+head (x :. _) = x
+tail (_ :. xs) = xs
+toList (x :. xs) = x : toList xs
 
 class Pointed a where bottom :: a
-instance Pointed a => Later (Stream a) where later = Cons bottom
-instance Merge2 a => Merge2 (Stream a) where
-  merge2 (Cons x xs) (Cons y ys) = Cons (merge2 x y) (merge2 xs ys)
+instance Pointed a => Later (Stream a) where later = (bottom :.)
+instance Semigroup a => Semigroup (Stream a) where (<>) = liftA2 (<>)
+instance (Pointed a, Semigroup a) => Ocpo (Stream a) where
+
+-- MaxInt
+newtype Max = Max { getMax :: Int } deriving (Num, Eq, Ord)
+instance Show Max where show = show . getMax
+instance Pointed Max where bottom = 0
+instance Semigroup Max where Max x <> Max y = Max (max x y)
+
+-- Okay, let's see if this works.
+nats :: Stream Max
+nats = fmap (1 +) $ later nats
